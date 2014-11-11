0c95efac7246f1fb3f5e9b632d0d51e6500c7bd8
Wendy Jansson <wendyjan@buffalo.edu>
2014-11-11 17:04:12 -0500

    Real version of store v1

---
+++ store1.py
@@ -0,0 +1,13 @@
+"""
+Sample code for Intro to Python, Class 2.
+
+This shows how to define a class.
+
+Nothing should happen if you run this file!
+"""
+
+class Store:
+
+    def __init__(self, newName):
+
+        self.name = newNames
