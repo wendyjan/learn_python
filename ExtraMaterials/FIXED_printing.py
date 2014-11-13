f1299038c23df87d17bb9f4c931c7082faf1cfe8
Wendy Jansson <wendyjan@buffalo.edu>
2014-11-13 17:54:45 -0500

    Files for study session

---
+++ ExtraMaterials/FIXED_printing.py
@@ -0,0 +1,33 @@
+"""
+Filename: FIXED_printing.py
+
+Author: Wendy Jansson
+
+
+
+This file shows FIXED code.
+
+Check the comments for the solutions. 
+
+The broken code is posted to my github:
+go to https://github.com/wendyjan/learnPython,
+and look in the folder "ExtraMaterial".
+
+
+"""
+
+if __name__ == "__main__":
+
+    # Use quotation marks to make "Dark Chocolate" a string.
+    # Otherwise, Python thinks it is a variable that you forgot to name. 
+    favoriteFoods = ["Apples", "Beets", "Dark Chocolate", "Grapes"]
+
+    # Don't forget to use parentheses with "print()" in Python 3!
+    print(favoriteFoods)
+
+    # "Invalid syntax" means you forgot the colon at the end of your if statement.
+    for x in favoriteFoods:
+        print(x) 
+    
+
+    
