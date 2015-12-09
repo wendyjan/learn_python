9eb341ab462124825eac248c191b78d2b486aa9c
Anne Izydorczak <anneizydorczak@gmail.com>
2015-12-08 19:35:06 -0500

    "Update Types Overview Lesson Code"

--- gdi_buffalo_2015/class1/Types_overview.py
+++ gdi_buffalo_2015/class1/Types_overview.py
@@ -6,3 +6,74 @@ type(x)
 x = str(x)
 type(x)
 x
+
+50 * 2
+‘50’ * 2
+
+5+5
+True + True
+
+hamburger[4:6]
+[10,11,12,13,14,15,16,17][4:6]
+
+L = []				#empty list
+L.append(“So”)
+L
+L.append(“what?”)
+L
+
+x = “show”
+x.append(“er”)
+x = “show” + (“er”)
+
+sum = sum + 1   #is equivalent to 
+sum += 1
+
+40/3
+
+5 == 6
+5 == ‘5’
+6 >= 7
+
+5 != 1 or 5 == 1
+4 < 5 and 1 < 4
+1 < 4 < 5
+a = 4
+1 < a < 5
+1 + 9 == 10 and 90 / 2 = 40
+not 5 == 1
+
+print (r”C:\py\code”)
+
+“That’s my boy,” said mom.
+“The apple don’t fall too far from the tree,” said dad.
+
+S = “Happy Halloween, Best Witches!”
+len(S)
+S * 2
+S + “Boo”
+S
+S[5]
+S[5:10]
+S.find(’Witch’)
+S.replace(‘!’, ‘?’)
+S.split(‘ ‘)
+S.lower()
+S
+
+my_string = “gluten free”
+my_string[0]
+my_string[3:9]
+my_string[:]
+“gluten free”[3]
+my_string[0:9]
+my_string[:7]
+my_string[2:]
+my_string[-1:-5]
+my_string[0:11:3]
+my_string[::-1]
+
+
+“free”
+“u”
+“tnf”
