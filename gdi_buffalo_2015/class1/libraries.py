bcbded69398c22f9c40fbd1fb05c2ac21cc556a2
Izydorczak <anneizydorczak@gmail.com>
2016-01-09 14:47:13 -0500

    Create libraries.py

---
+++ gdi_buffalo_2015/class1/libraries.py
@@ -0,0 +1,27 @@
+import turtle
+my_window = turtle.Screen()
+Paula = turtle.Turtle()
+Paula.forward(150)
+my_window.exitonclick()
+
+import turtle as tur
+my_window = tur.Screen()
+Paula = tur.Turtle()
+Paula.forward(150)
+my_window.exitonclick()
+
+from turtle import Screen, Turtle, forward
+my_window = Screen()
+Paula = Turtle()
+Tom = Turtle()
+Paula.forward(150)
+Tom.forward(50)
+my_window.exitonclick()
+
+from turtle import *
+my_window = Screen()
+Paula = Turtle()
+Tom = Turtle()
+Paula.forward(150)
+Tom.forward(50)
+my_window.exitonclick()
