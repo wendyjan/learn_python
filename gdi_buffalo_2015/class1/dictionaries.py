505b0335dd72f83ae02a4a6f6cb01a08adc6df49
wendyjan <wendyjan@buffalo.edu>
2016-01-12 20:52:08 -0500

    Included more examples of adding and removing for dictionaries

--- gdi_buffalo_2015/class1/dictionaries.py
+++ gdi_buffalo_2015/class1/dictionaries.py
@@ -6,16 +6,21 @@ This file shows dictionaries in Python.
 """
 
 if __name__ == "__main__":
+    # Make a new dictionary, using { instead of [.
+    # Each pair of (key, value) items is connected with a colon.
     my_dict = { "Wendy": "Jansson", "Joe": "Jones", "Clara": "Rodriquez"}
     print my_dict
-
+    
+    # Add a new person to our dictionary.
+    my_dict["Carey"] = "Forester"
+    print my_dict["Carey"]
+    print my_dict
+    
+    # Can we store two different Claras?
     print my_dict["Joe"]
     my_dict["Clara"] = "Wu"
     print my_dict["Clara"]
 
-    my_dict["Joe"] = "Forester"
-    print my_dict["Joe"]
-    print my_dict
 
     #print my_dict["Anne"] # Comment this line out unless you want an error!
 
@@ -23,3 +28,11 @@ if __name__ == "__main__":
     monty = {"age": 3, "name": "Monty", "type": "doberman"}
     for k in monty.keys():
         print k, ":", monty[k]
+    
+    # Store employees and update the dictionary when they come and go.
+    emps = {1: "Rich Simons", 2: "Natalia Burges", 3: "Adrianne Velasquez", 4: "Ying Su"}
+    print emps
+    print "There are", len(emps), "total at the start."
+    emps[5] = "Thomas Hah"
+    emps.remove(3)
+    print "After some changes, the employee list is:", emps.values()
