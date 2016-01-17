bada774ce8307b3c7b70eccbf81e40463d1b6f41
Izydorczak <anneizydorczak@gmail.com>
2016-01-17 11:01:11 -0500

    Update file_io.py

--- gdi_buffalo_2015/class2/file_io.py
+++ gdi_buffalo_2015/class2/file_io.py
@@ -17,3 +17,5 @@ if __name__ == "__main__":
             f.write(line)
     print 'All done!'
 
+# which functions or methods are coming from os? Would assigning f = open ... be the same as the with-as? is there another purpose for the with- as?
+# It took me awhile (in the past) to figure out that when you do things like this, your file, the new file and the program file all need to be in the same directory.
