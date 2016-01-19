bcc2ad0a39f9be5d5e803d721ad523e579dfbc31
wendyjan <wendyjan@buffalo.edu>
2016-01-18 21:58:40 -0500

    Incorporated helpful comments from Anne.

--- gdi_buffalo_2015/class2/file_io.py
+++ gdi_buffalo_2015/class2/file_io.py
@@ -1,21 +1,26 @@
+"""
+filename: file_io.py
+author: wendyjan
 
-import os
-
+This file shows a simple file input/output example.
+"""
 
 if __name__ == "__main__":
     
     # Read in a file.
+    # Note that your input file and the program file need to be in the same directory!
     with open("input.txt", 'r') as f:
         text = f.readlines()
     print text
 
     # Write that to a new file, with a change!
+    # The output file will be written to the same directory.
     for i, line in enumerate(text):
         text[i] = line.upper()
     with open("output.txt", 'w') as f:
+        # Use the 'with' keyword to protect a resource.
+        # Even if an error occurs within the 'with' block,
+        # this way the file will still be closed properly!
         for line in text:
             f.write(line)
     print 'All done!'
-
-# which functions or methods are coming from os? Would assigning f = open ... be the same as the with-as? is there another purpose for the with- as?
-# It took me awhile (in the past) to figure out that when you do things like this, your file, the new file and the program file all need to be in the same directory.
